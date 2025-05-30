a93fb4b9f5fe0a19b89d6a16fb1d341e3abc880c
root <root@debian-BULLSEYE-live-builder-AMD64>
2025-05-31 00:20:10 +0800

    20250531

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["server1.wenjie1231.com"];
+pub const RS_PUB_KEY: &str = "o3KCqxx0MP3oQpFI2aEypnfs+a7g3YdQsQbBgcva1po=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
